96b28fb5604ab969a7e74747f2095095e1e16462
Abdallah badawi metwally eissa <abdallahbadawimetwallyeissa@Abdallahs-MacBook-Air.local>
2021-11-04 06:30:40 +0200

    adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angTest';
+isActive:boolean=false;
+isActivedecount:boolean=false;
+Count:number=1;
+deCount:number=20;
+Increment(){
+
+
+  this.Count++;
+  this.isActive=true;
+  if(this.deCount>20){
+    this.deCount--;
+    this.isActive=true;
+
+
+  }
+
+
+}
+decrement(){
+  this.deCount++;
+  this.isActivedecount=true;
+  
+  if(this.Count>1){
+    this.Count--;
+    this.isActive=false;
+
+
+  }
+  }
+  
 }
